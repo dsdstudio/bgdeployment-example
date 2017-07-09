bd26afe831abe8d839c22ff5311df18b0ac6c0f1
보롱킴 <dsdgun@gmail.com>
2017-07-09 23:09:54 +0900

    add sample project

---
+++ apps/src/main/java/net/dsdstudio/bluegreentest/BluegreentestApplication.java
@@ -0,0 +1,12 @@
+package net.dsdstudio.bluegreentest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluegreentestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluegreentestApplication.class, args);
+	}
+}
